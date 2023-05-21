49fe4cf8bf62a8f3da33460b2a17dc1bdaf8a92b
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-05-21 12:57:21 -0300

    continue reactyt

--- 04-secret-word/src/App.jsx
+++ 04-secret-word/src/App.jsx
@@ -23,12 +23,48 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState("");
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category);
+
+    // pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+    console.log(word);
+
+    return { word, category };
+  };
+
   // Starts the secret word game
   const StartGame = () => {
+    // Pick word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((letra) => letra.toLowerCase());
+    console.log(wordLetters);
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
 
-  //
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
 
   return (
     <div className="App">
