8e8fd5627bf766fd42d16301124621c1af78e42f
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-06-14 23:59:41 -0300

    continue hooks adv

---
+++ 08-hooksAvancado/src/components/HookUseCallBack.jsx
@@ -0,0 +1,22 @@
+import { useCallback, useState } from "react";
+
+import List from "./List";
+
+const HookUseCallBack = () => {
+  const [counter, setCounter] = useState(0);
+
+  const getItemsFromDataBase = useCallback(() => {
+    return ["a", "b", "c"];
+  }, []);
+
+  return (
+    <div>
+      <h1>useCallBack</h1>
+      <List getItems={getItemsFromDataBase} />
+      <button onClick={() => setCounter(counter + 1)}>Alterar</button>
+      <p>{counter}</p>
+    </div>
+  );
+};
+
+export default HookUseCallBack;
--- 08-hooksAvancado/src/pages/Home.jsx
+++ 08-hooksAvancado/src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import HookUseEffect from "../components/HookUseEffect";
-import HookUseReducer from "../components/HookUseReducer";
-import HookUseState from "../components/HookUseState";
-
 // useContext
 import { useContext } from "react";
 import { SomeContext } from "../components/HookUseContext";
+
+// Hooks
+import HookUseEffect from "../components/HookUseEffect";
+import HookUseReducer from "../components/HookUseReducer";
+import HookUseState from "../components/HookUseState";
 import HookUseRef from "../components/HookUseRef";
+import HookUseCallBack from "../components/HookUseCallBack";
+import HookUseMemo from "../components/HookUseMemo";
+import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
 
 const Home = () => {
   const { contextValue } = useContext(SomeContext);
@@ -22,6 +26,12 @@ const Home = () => {
       <p>Valor do contexto: {contextValue}</p>
       <hr />
       <HookUseRef />
+      <hr />
+      <HookUseCallBack />
+      <hr />
+      <HookUseMemo />
+      <hr />
+      <HookUseLayoutEffect />
     </div>
   );
 };
